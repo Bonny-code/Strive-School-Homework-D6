055c8a3aa87eea9e8831bd3d88f359bc06cc20ae
Bonny-code <ndiboniface@outlook.com>
2021-06-28 11:15:39 +0100

    ex B done

--- d6.js
+++ d6.js
@@ -19,7 +19,8 @@
 /* Ex.B
     Create a variable called "sum" and assign to it the result of the sum between 10 and 20.
 */
-
+  let sum = 10 + 20
+  console.log(sum)
 /* Ex.C 
     Create a variable called "random" and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
 */
